e05b17352bb554b2bfc21bdb3db0d83d5bea0f05
Nerces Kahwajian <N/A>
2018-06-15 15:42:42 +0800

    First Commit
    Can get list of currently available offers

---
+++ main.py
@@ -0,0 +1,39 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+
+def getOfferList:
+	
+	page = requests.get("https://www.americanexpress.com/au/network/shopping.html")
+
+	soup = BeautifulSoup(page.content, 'html.parser')
+
+	offerList = []
+
+	for elem in soup.find_all('a', href=re.compile("offer=")):
+		offerList.append(elem['trackingid'])
+	
+	return offerList
+
+def getOfferDetails:
+	
+	titles = []
+	description = []
+
+	for i in offerList:
+		page = requests.get("https://www.americanexpress.com/au/network/shopping/doe-offer-detail.html?offer=" + i)
+		soup = BeautifulSoup(page.content, 'html.parser')
+
+
+
+
+
+
+
+
+
+def main():
+
+	offerList = getOfferList
+
+	getOfferDetails(offerList)
